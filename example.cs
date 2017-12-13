bff9b172f46ba39812289815520f9ccef3a5c55c
Kevin-B47 <kblock@student.sjcny.edu>
2017-12-12 20:02:55 -0500

    Removed junk

--- example.cs
+++ example.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-
 using Android.App;
 using Android.Content.PM;
 using Android.Runtime;
@@ -8,37 +7,19 @@ using Android.Widget;
 using Android.OS;
 using System.Collections.Generic;
 
-namespace DefuseBomb.Droid
-{
-    [Activity(Label = "lbl", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
-    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
-    {
-        protected override void OnCreate(Bundle bundle)
-        {
-            TabLayoutResource = Resource.Layout.Tabbar;
-            ToolbarResource = Resource.Layout.Toolbar;
 
-            base.OnCreate(bundle);
+public async void GetDataAsync() {
 
-            global::Xamarin.Forms.Forms.Init(this, bundle);
-            LoadApplication(new App());
-            GetDataAsync();
-        }
+	Dictionary<string, string> trail = new Dictionary<string, string>();
 
-        public async void GetDataAsync() {
+	trail.Add("postKey", "postvalue");
+	trail.Add("password", "secretpassword");
 
-            Dictionary<string, string> trail = new Dictionary<string, string>();
+	var stuff = await REST.GetSimpleData("https://www.titsrp.com/boombox/fetch.php?");
+	Console.WriteLine(stuff);
 
-            trail.Add("postKey", "postvalue");
-            trail.Add("password", "secretpassword");
+	var post = await REST.PostSimpleData("http://httpbin.org/post", trail);
+	Console.WriteLine(post);
 
-            var stuff = await REST.GetSimpleData("https://www.titsrp.com/boombox/fetch.php?");
-            Console.WriteLine(stuff);
-
-            var post = await REST.PostSimpleData("http://httpbin.org/post", trail);
-            Console.WriteLine(post);
-
-        }
-    }
 }
 
