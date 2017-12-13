cc31469027409a06c8d2f20fbc9d3dcc3a43a8ac
Kevin-B47 <kblock@student.sjcny.edu>
2017-12-12 19:58:38 -0500

    First commit

---
+++ REST.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using ModernHttpClient;
+
+namespace TestApp.Droid {
+    class REST {
+
+        private static HttpClient Client = new HttpClient(new NativeMessageHandler());
+
+       public static async Task<string> GetSimpleData(string url) {
+            var response = await Client.SendAsync(new HttpRequestMessage(HttpMethod.Get, new Uri(url)));
+            if (response.IsSuccessStatusCode) {
+                try {
+                   return await response.Content.ReadAsStringAsync();
+                }catch(HttpRequestException e) {
+                    return e.Message;
+                }
+                
+            }
+            return "-1";
+        }
+
+        public static async Task<string> PostSimpleData(string url, Dictionary<string, string> body) {
+
+            var content = new FormUrlEncodedContent(body.ToList());
+
+            var response = await Client.PostAsync(new Uri(url), content);
+
+            if (response.IsSuccessStatusCode) {
+                try {
+                    return await response.Content.ReadAsStringAsync();
+                }catch(HttpRequestException e) {
+                    return e.Message;
+                }
+            }
+            else {
+                return "-1";
+            }
+
+
+        }
+
+    }
+}
---
+++ example.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+using Android.App;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using System.Collections.Generic;
+
+namespace DefuseBomb.Droid
+{
+    [Activity(Label = "lbl", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            TabLayoutResource = Resource.Layout.Tabbar;
+            ToolbarResource = Resource.Layout.Toolbar;
+
+            base.OnCreate(bundle);
+
+            global::Xamarin.Forms.Forms.Init(this, bundle);
+            LoadApplication(new App());
+            GetDataAsync();
+        }
+
+        public async void GetDataAsync() {
+
+            Dictionary<string, string> trail = new Dictionary<string, string>();
+
+            trail.Add("postKey", "postvalue");
+            trail.Add("password", "secretpassword");
+
+            var stuff = await REST.GetSimpleData("https://www.titsrp.com/boombox/fetch.php?");
+            Console.WriteLine(stuff);
+
+            var post = await REST.PostSimpleData("http://httpbin.org/post", trail);
+            Console.WriteLine(post);
+
+        }
+    }
+}
+
